3f9e4c8039fbf132f2232a22d0a1f92e54854d6d
sarah <sarah.peng@berkeley.edu>
2023-10-01 15:53:44 -0700

    exhibit preview draft

--- src/app/dummyPage/page.tsx
+++ src/app/dummyPage/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
-import ExhibitPreview from "@/components/userComponents/ExhibitPreview/ExhibitPreview";
+import Image from 'next/image';
+import ExhibitPreview from '@/components/userComponents/ExhibitPreview/ExhibitPreview';
 
 export default function Home() {
   return (
     <ExhibitPreview
-      name="Exhibit Name Ryan"
-      location="location description"
-      description="description"
+      name="Exhibit Name"
+      location="Location description here"
+      description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
       about="About this exhibit"
+      topimage="/dog.jpg"
+      bottomimage="/dog2.jpg"
     />
   );
 }
