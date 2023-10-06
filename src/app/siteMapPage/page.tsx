4d3ea41d7bdec052d2ffbfdd9789cc57e9c9646d
Chris Torres <99312117+christophertorres1@users.noreply.github.com>
2023-10-05 23:01:40 -0700

    Christophertorres/phs 5 tours homescreen (#4)
    * feat: navbar
    
    * draft of exhibit preview modal. i'm just commiting for now so i can pull from the main again
    
    * exhibit preview draft
    
    * exhibit previewgit add .
    
    * resolve merge conflict
    
    * resolved merge conflict
    
    * exhibit previewgit add .
    
    * feat: buttons on homeAdmissionPage, styled pages
    
    * supabase restructuring
    
    * supabase restructure II
    
    * made client.ts cleaner
    
    * rebase
    
    * exhibit preview draft
    
    * rearranged file system
    
    ---------
    
    Co-authored-by: sarah <sarah.peng@berkeley.edu>
    Co-authored-by: Andrei Tan <ManTan.875@berkeley.edu>
    Co-authored-by: Andrei Tan <73627326+andreisito@users.noreply.github.com>

--- src/app/siteMapPage/page.tsx
+++ src/app/siteMapPage/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
 import React from 'react';
-import NavBar from '../components/userComponents/navBar/navBar';
+import NavBar from '../../components/userComponents/navBar/navBar';
 
-function Home() {
+function App() {
   return (
     <div style={{ backgroundColor: '#ebf0e4', height: '100vh' }}>
       <NavBar />
       <div style={{ padding: '16px' }}>
-        <h1 style={{ color: '#333333', fontSize: '2rem', fontWeight: 700 }}>Home</h1>
+        <h1 style={{ color: '#333333', fontSize: '2rem', fontWeight: 700 }}>Site Map</h1>
       </div>
     </div>
   );
 }
 
-export default Home;
+export default App;
---
+++ src/supabase/client.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Check if the environment variables are defined
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Supabase environment variables are not defined.');
+}
+
+// Initialize the Supabase client
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
+export default supabase;
