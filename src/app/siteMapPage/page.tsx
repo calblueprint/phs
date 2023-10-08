0041f23282be72e0fc4d4eddaaea9fb9a52a8030
Noah Hernandez <63211322+oahnh@users.noreply.github.com>
2023-10-08 04:02:14 -0700

    feat(sitemap): adds map and filter buttons to sitemap page

--- src/app/siteMapPage/page.tsx
+++ src/app/siteMapPage/page.tsx
@@ -1,17 +1,55 @@
 'use client';
 
 import React from 'react';
+import dynamic from 'next/dynamic';
 import NavBar from '../../components/userComponents/navBar/navBar';
 
-function App() {
+const filterButtonContent: string[] = [
+  'Buildings & Services',
+  'Pools, Aviaries, Enclosures',
+  'Site Features',
+];
+
+const SiteMap = dynamic(
+  () => import('../../components/userComponents/SiteMap/SiteMap'),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  },
+);
+
+function MapPage() {
+  // TODO: ask about expected styling when button selected – able to filter on multiple conds?
   return (
-    <div style={{ backgroundColor: '#ebf0e4', height: '100vh' }}>
+    <>
       <NavBar />
-      <div style={{ padding: '16px' }}>
-        <h1 style={{ color: '#333333', fontSize: '2rem', fontWeight: 700 }}>Site Map</h1>
+      <div className="flex flex-col items-start pl-6">
+        <div className="container mb-6 mt-9">
+          <h1 style={{ fontSize: '2rem', fontWeight: 700 }}>Site Map</h1>
+          <div className="inline-flex flex-row items-center gap-x-2.5 w-full overflow-x-scroll no-scrollbar">
+            {filterButtonContent &&
+              filterButtonContent.map(text => (
+                <button
+                  key={text}
+                  type="button"
+                  className="bg-[#4b711d]/[0.13] text-white py-2 px-4 rounded-full whitespace-nowrap"
+                  style={{
+                    border: '1px solid #4B711D',
+                    flexWrap: 'nowrap',
+                    color: '#4B711D',
+                  }}
+                >
+                  {text}
+                </button>
+              ))}
+          </div>
+        </div>
+        <div className=" w-full pr-6 flex h-2/3 mb-6">
+          <SiteMap />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export default App;
+export default MapPage;
---
+++ src/components/userComponents/SiteMap/SiteMap.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { LatLngExpression } from 'leaflet';
+import React from 'react';
+import {
+  LayersControl,
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  ZoomControl,
+} from 'react-leaflet';
+
+const center: LatLngExpression = {
+  lat: 37.25323057233323,
+  lng: -122.08556629289924,
+};
+
+const tileLayer: { attribution: string; url: string } = {
+  attribution:
+    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+};
+
+function SiteMap() {
+  return (
+    <MapContainer
+      center={center}
+      zoom={18}
+      zoomControl={false}
+      scrollWheelZoom
+      style={{ height: '75vh', width: '100%', minHeight: '544px' }}
+    >
+      <ZoomControl position="bottomright" />
+      <TileLayer {...tileLayer} />
+      <LayersControl position="topright">
+        <LayersControl.Overlay name="Marker with popup">
+          <Marker position={center}>
+            <Popup>
+              Populate the popups here <br /> Add new control overlays for
+              different locations
+            </Popup>
+          </Marker>
+        </LayersControl.Overlay>
+      </LayersControl>
+    </MapContainer>
+  );
+}
+
+export default SiteMap;
