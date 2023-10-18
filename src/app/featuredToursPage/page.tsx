c91a217a2cb541801e8830256b3e164d2b9b682a
Chris Torres <christophertorres@berkeley.edu>
2023-10-18 00:04:30 -0700

    solved merge conflict

--- src/app/featuredToursPage/page.tsx
+++ src/app/featuredToursPage/page.tsx
@@ -1,16 +1,84 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 import NavBar from '../../components/userComponents/navBar/navBar';
 
 function App() {
   return (
-    <div style={{ backgroundColor: '#ebf0e4', height: '100vh' }}>
+    <div className="bg-[#ebf0e4]">
       <NavBar />
-      <div style={{ padding: '16px' }}>
-        <h1 style={{ color: '#333333', fontSize: '2rem', fontWeight: 700 }}>
+      <div className="p-4">
+        <h1 className="text-[#333333] text-3xl font-bold mb-4">
           Featured Tours
         </h1>
+        <p className="text-[#333333] mb-4">
+          Embark on a tour and explore our exhibits from anywhere - at home or in person!
+        </p>
+
+        <div className="flex flex-row justify-between mb-4">
+          <button
+            type="button"
+            className="bg-[#d7e0cc] rounded-2xl p-4 w-[48%] h-[150px]">
+            <Link href="/">
+              <h1 className="text-[#333333] p-4">
+                Tour Name
+              </h1>
+            </Link>
+          </button>
+          <button
+            type="button"
+            className="bg-[#d7e0cc] rounded-2xl p-4 w-[48%] h-[150px]">
+            <Link href="/">
+              <h1 className="text-[#333333] p-4">
+                Tour Name
+              </h1>
+            </Link>
+          </button>
+        </div>
+
+        <div className="flex flex-row justify-between mb-4">
+          <button
+            type="button"
+            className="bg-[#d7e0cc] rounded-2xl p-4 w-[48%] h-[150px]">
+            <Link href="/">
+              <h1 className="text-[#333333] p-4">
+                Tour Name
+              </h1>
+            </Link>
+          </button>
+          <button
+            type="button"
+            className="bg-[#d7e0cc] rounded-2xl p-4 w-[48%] h-[150px]">
+            <Link href="/">
+              <h1 className="text-[#333333] p-4">
+                Tour Name
+              </h1>
+            </Link>
+          </button>
+        </div>
+
+        <div className="flex flex-row justify-between mb-4">
+          <button
+            type="button"
+            className="bg-[#d7e0cc] rounded-2xl p-4 w-[48%] h-[150px]">
+            <Link href="/">
+              <h1 className="text-[#333333] p-4">
+                Tour Name
+              </h1>
+            </Link>
+          </button>
+          <button
+            type="button"
+            className="bg-[#d7e0cc] rounded-2xl p-4 w-[48%] h-[150px]">
+            <Link href="/">
+              <h1 className="text-[#333333] p-4">
+                Tour Name
+              </h1>
+            </Link>
+          </button>
+        </div>
+
       </div>
     </div>
   );
