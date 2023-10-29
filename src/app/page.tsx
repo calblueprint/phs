02f60be2c9338c1e0c24cb296b5aa6169bdae0c1
Andrei Tan <73627326+andreisito@users.noreply.github.com>
2023-10-29 13:32:59 -0700

    styling ok on phones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,24 @@
 import React from 'react';
 import NavBar from '../components/userComponents/navBar/navBar';
 
+/**
+ *
+ */
 function Home() {
   return (
     <div style={{ backgroundColor: '#ebf0e4', height: '100vh' }}>
       <NavBar />
-      <div style={{ padding: '16px' }}>
-        <h1 style={{ color: '#333333', fontSize: '2rem', fontWeight: 700 }}>
-          Home
-        </h1>
+      <div className="relative w-96 h-96 bg-cover bg-center" style={{ backgroundImage: `url(https://qkkuacqtcsfjbnzmxmhk.supabase.co/storage/v1/object/public/images/edisonImage.png)` }}>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="text-white text-center">
+          <p className="text-xl font-bold">Welcome to </p>
+          <p className="text-sm">Ellison Conservation Center for Wildlife Care</p>
+          <div className="w-44 h-11 px-4 py-3 bg-asparagus rounded-xl justify-center items-center gap-2.5 inline-flex">
+            <button type = "button" className="text-center text-neutral-50 text-base font-bold font-['Lato']">Start a Tour</button>
+          </div>
+        </div>
       </div>
+    </div>
     </div>
   );
 }
