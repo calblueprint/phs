648ef68c12bfc6a438a699189d2fe1af9ec2c534
Andrei Tan <73627326+andreisito@users.noreply.github.com>
2023-11-02 18:06:45 -0700

    fixed global styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ function Home() {
     <div style={{ backgroundColor: '#ebf0e4', height: '100vh' }}>
       <NavBar />
       <div
-        className="relative w-96 h-96 bg-cover bg-center"
+        className="relative w-full h-96 bg-cover bg-center"
         style={{
           backgroundImage: `url(https://qkkuacqtcsfjbnzmxmhk.supabase.co/storage/v1/object/public/images/edisonImage.png)`,
         }}
@@ -19,14 +19,11 @@ function Home() {
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="text-white text-center">
             <p className="text-xl font-bold">Welcome to </p>
-            <p className="text-sm">
+            <p className="w-80 text-center text-smoke text-3xl font-bold font-['Lato']">
               Ellison Conservation Center for Wildlife Care
             </p>
             <div className="w-44 h-11 px-4 py-3 bg-asparagus rounded-xl justify-center items-center gap-2.5 inline-flex">
-              <button
-                type="button"
-                className="text-center text-neutral-50 text-base font-bold font-['Lato']"
-              >
+              <button type="button" className="text-center text-smoke text-base font-bold font-['Lato']">
                 Start a Tour
               </button>
             </div>
