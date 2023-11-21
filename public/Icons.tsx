11d1b8cc544d9dc6e7f8b08f31f884d84a22438a
Chris Torres <christophertorres@berkeley.edu>
2023-11-20 17:24:22 -0800

    added file to store icons

---
+++ public/Icons.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+/**
+ * @returns the HamburgerMenu icon
+ */
+export function HamburgerMenu() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+    >
+      <path
+        d="M20.0007 28.3332L31.6673 28.3332M8.33398 19.9998H31.6673M8.33398 11.6665H31.6673"
+        stroke="#FFFDF7"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+/**
+ * @returns the CloseMenu icon
+ */
+export function CloseMenu() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+    >
+      <path
+        d="M21.8754 21.8749L3.12549 3.125M21.8755 3.125L3.12549 21.8751"
+        stroke="#FFFDF7"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
