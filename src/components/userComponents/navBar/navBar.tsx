100b8346109a608bc5d91c8036575f6b7510eb0c
Chris Torres <christophertorres@berkeley.edu>
2023-11-21 18:42:32 -0800

    style: navBar hifi styling

--- src/components/userComponents/navBar/navBar.tsx
+++ src/components/userComponents/navBar/navBar.tsx
@@ -16,21 +16,23 @@ export default function NavBar() {
   }
 
   return (
-    <nav className="bg-scary-forest p-4 flex sticky items-center justify-between z-[9999]">
-      <Link href="/">
-        <img
-          src="https://phs-spca.org/wp-content/uploads/2017/03/PHSLogo.jpg"
-          alt="PHSLogo"
-          className="object-contain max-w-[50%]"
-        />
-      </Link>
-      <div className="flex-grow" />
-      <button type="button" className="w-10 h-10" onClick={handleClick}>
-        <HamburgerMenu />
-      </button>
+    <nav className="bg-scary-forest w-[24.375rem] h-[7rem] flex flex-col relative z-[9999]">
+      <div className='flex flex-row justify-between relative top-[3.25rem]'>
+        <Link href="/">
+          <img
+            src="https://phs-spca.org/wp-content/uploads/2017/03/PHSLogo.jpg"
+            alt="PHSLogo"
+            className="object-contain w-[6.5rem] h-[2.25rem] relative left-[1.13rem]"
+          />
+        </Link>
+        <button type="button" className="w-10 h-10 relative right-[0.81rem]" onClick={handleClick}>
+          <HamburgerMenu />
+        </button>
+      </div>
+      <div className=" bg-hunter-green w-[24.375rem] h-[0.375rem] absolute bottom-0"/>
 
       {showMenu && (
-        <div className="fixed top-20 right-0 h-full w-3/5 bg-[#ebf0e4] shadow-lg">
+        <div className="fixed top-28 right-0 h-full w-3/5 bg-[#ebf0e4] shadow-lg">
           <h1 className="text-xl text-black font-bold p-4">WELCOME</h1>
           <ul className="p-4">
             <Link href="/" className="block mb-2 text-black">
