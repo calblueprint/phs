70478a377a51491a5c2db4541abe33af4df1d55e
Chris Torres <christophertorres@berkeley.edu>
2023-11-21 18:57:26 -0800

    style: side menu

--- src/components/userComponents/navBar/navBar.tsx
+++ src/components/userComponents/navBar/navBar.tsx
@@ -32,45 +32,45 @@ export default function NavBar() {
       <div className=" bg-hunter-green w-[24.375rem] h-[0.375rem] absolute bottom-0"/>
 
       {showMenu && (
-        <div className="fixed top-28 right-0 h-full w-3/5 bg-[#ebf0e4] shadow-lg">
-          <h1 className="text-xl text-black font-bold p-4">WELCOME</h1>
-          <ul className="p-4">
-            <Link href="/" className="block mb-2 text-black">
-              Home
-            </Link>
-          </ul>
+        <div className="relative">
+          <div className="fixed top-28 left-0 w-full h-full bg-black opacity-50"/>
+          <div className="fixed top-28 right-0 h-full w-3/5 bg-ivory shadow-lg">
+            <h1 className="text-xl text-scary-forest font-bold p-4">WELCOME</h1>
+            <ul className="p-4">
+              <Link href="/" className="block mb-2 text-night">
+                Home
+              </Link>
+            </ul>
 
-          <h1 className="text-xl text-black font-bold p-4">VISIT</h1>
-          <ul className="p-4">
-            <Link href="/hoursAdmissionPage" className="block mb-2 text-black">
-              Hours & Admission
-            </Link>
-            <Link href="/featuredToursPage" className="block mb-2 text-black">
-              Featured Tours
-            </Link>
-            <Link href="/spotlightPage" className="block mb-2 text-black">
-              Spotlight Tours
-            </Link>
-            <Link href="/siteMapPage" className="block mb-2 text-black">
-              Site Map
-            </Link>
-            <Link href="/qrCodeTourPage" className="block text-black">
-              QR Code Tour
-            </Link>
-            <Link href="/qrCodeScanner" className="block text-black">
-              QR Scanner
-            </Link>
-          </ul>
+            <h1 className="text-xl text-scary-forest font-bold p-4">VISIT</h1>
+            <ul className="p-4">
+              <Link href="/hoursAdmissionPage" className="block mb-2 text-night">
+                Hours & Admission
+              </Link>
+              <Link href="/featuredToursPage" className="block mb-2 text-night">
+                Featured Tours
+              </Link>
+              <Link href="/spotlightPage" className="block mb-2 text-night">
+                Spotlight Tours
+              </Link>
+              <Link href="/siteMapPage" className="block mb-2 text-night">
+                Site Map
+              </Link>
+              <Link href="/qrCodeTourPage" className="block text-night">
+                QR Code Tour
+              </Link>
+            </ul>
 
-          <h1 className="text-xl text-black font-bold p-4">LEARN & EXPLORE</h1>
-          <ul className="p-4">
-            <Link href="/collectionsPage" className="block mb-2 text-black">
-              Collections
-            </Link>
-            <Link href="/newsFeedPage" className="block mb-2 text-black">
-              News Feed
-            </Link>
-          </ul>
+            <h1 className="text-xl text-scary-forest font-bold p-4">LEARN & EXPLORE</h1>
+            <ul className="p-4">
+              <Link href="/collectionsPage" className="block mb-2 text-night">
+                Collections
+              </Link>
+              <Link href="/newsFeedPage" className="block mb-2 text-night">
+                News Feed
+              </Link>
+            </ul>
+          </div>
         </div>
       )}
     </nav>
