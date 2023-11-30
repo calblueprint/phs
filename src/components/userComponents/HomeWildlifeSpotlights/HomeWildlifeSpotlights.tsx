f61e2309d7e52be8c93a91060218fc4162723c99
Andrei Tan <73627326+andreisito@users.noreply.github.com>
2023-11-30 12:22:12 -0800

    chore: commented out related spotlight for now

--- src/components/userComponents/HomeWildlifeSpotlights/HomeWildlifeSpotlights.tsx
+++ src/components/userComponents/HomeWildlifeSpotlights/HomeWildlifeSpotlights.tsx
@@ -14,10 +14,11 @@ import { SpotlightWithMediaRow } from '../../../types/types';
  * See All button will lead to the spotlights home page
  * Spotlight scroller will showcase and link to chosen spotlights
  */
-function HomeWildlifeSpotlights(
-  // {spotlightsWithMedia} : {spotlightsWithMedia : SpotlightWithMediaRow[]}
-  ) {
-  const [spotlightsWithMedia, setSpotlightsWithMedia] = useState<SpotlightWithMediaRow[]>([]);
+function HomeWildlifeSpotlights() {
+// {spotlightsWithMedia} : {spotlightsWithMedia : SpotlightWithMediaRow[]}
+  const [spotlightsWithMedia, setSpotlightsWithMedia] = useState<
+    SpotlightWithMediaRow[]
+  >([]);
   useEffect(() => {
     /**
      * @returns - !!! WIP !!! Used to fetch data for the spotlights with media
@@ -27,7 +28,7 @@ function HomeWildlifeSpotlights(
         const responseData = await joinSpotlightsWithMedia();
         setSpotlightsWithMedia(responseData);
       } catch (error) {
-        console.error("Error in fetch data: ", error);
+        console.error('Error in fetch data: ', error);
       }
     }
     fetchData();
@@ -39,14 +40,14 @@ function HomeWildlifeSpotlights(
         <h3 className="text-night ml-4 ">Our Wildlife Spotlights</h3>
         <Link
           className="b1 text-asparagus inline-flex items-center mr-4"
-          href = "/spotlightPage"
+          href="/spotlightPage"
         >
           See All
           <HiChevronRight className="text-2xl" />
         </Link>
       </div>
       <div className="carousel carousel-center max-w-md p-4 space-x-4 rounded-box w-full">
-        {spotlightsWithMedia.map((spotlight : SpotlightWithMediaRow) => (
+        {spotlightsWithMedia.map((spotlight: SpotlightWithMediaRow) => (
           <div
             key={spotlight.id}
             className="carousel-item w-[234.78px] h-[275px] bg-gradient-to-b from-zinc-800 to-black rounded-lg overflow-hidden"
@@ -57,13 +58,15 @@ function HomeWildlifeSpotlights(
               alt="background for spotlight"
             />
             <div className="bg-red-400 flex-col justify-start items-start gap-2 inline-flex">
-              <div className="w-[181px] text-stone-50 text-xl font-extrabold font-['Lato']">Dove Flight Enclosures</div>
-              <div className="w-[181px] text-stone-50 text-sm font-normal font-['Lato']">Species diversity, reproduction, and conservation</div>
+              <div className="w-[181px] text-stone-50 text-xl font-extrabold font-['Lato']">
+                Dove Flight Enclosures
+              </div>
+              <div className="w-[181px] text-stone-50 text-sm font-normal font-['Lato']">
+                Species diversity, reproduction, and conservation
+              </div>
             </div>
           </div>
-            )
-        )
-        }
+        ))}
       </div>
     </div>
   );
