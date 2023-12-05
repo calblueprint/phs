5600794437a25a6a6d037dd840bbb085a2446540
Chris Torres <christophertorres@berkeley.edu>
2023-12-05 10:12:05 -0500

    style: hifi design updates

--- src/components/userComponents/BackButton/BackButton.tsx
+++ src/components/userComponents/BackButton/BackButton.tsx
@@ -6,7 +6,7 @@ import { BackArrow } from '../../../../public/Icons';
  */
 export default function BackButton() {
   return (
-    <div className="bg-[#EBF0E8] w-[3rem] h-[2.0625rem] rounded-lg flex items-center justify-around">
+    <div className="bg-mint-cream w-[3rem] h-[2.0625rem] rounded-lg flex items-center justify-around">
       <BackArrow />
     </div>
   );
