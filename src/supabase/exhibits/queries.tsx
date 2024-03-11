ed08d41c5510f8c01d6cacd2fdb4233ce7f2248c
jxmoose <justin.xue7@gmail.com>
2024-03-10 17:57:20 -0700

    added database and page

---
+++ src/supabase/exhibits/queries.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { ExhibitRow } from '../../types/types';
+import supabase from '../client';
+
+/**
+ *
+ */
+export async function fetchAllExhibits() {
+  const { data, error } = await supabase.from('exhibits').select('*');
+  if (error) {
+    throw new Error(`An error occurred trying to read exhibits: ${error}`);
+  }
+  return data;
+}
+
+/**
+ *
+ * @param id
+ */
+export async function deleteDisplay(id: string) {
+  const { error } = await supabase.from('exhibits').delete().eq('id', id);
+
+  if (error) {
+    throw new Error(`An error occurred trying to delete displays: ${error}`);
+  } else {
+    fetchAllExhibits();
+  }
+}
+
+/**
+ *
+ * @param exhibitData
+ */
+export async function createExhibit(exhibitData: ExhibitRow) {
+  const { data, error } = await supabase.from('exhibits').upsert([exhibitData]);
+
+  if (error) {
+    throw new Error(
+      `An error occurred trying to create displays: ${error.message}`,
+    );
+  }
+  const newExhibit = data;
+  return newExhibit;
+}
+
+/**
+ *
+ * @param id - id number
+ * @param updatedInfo - the exhibit row to update
+ * @returns - updates that given exhibit row
+ */
+export async function updateExhibit(id: number, updatedInfo: ExhibitRow) {
+  const { data, error } = await supabase
+    .from('exhibits')
+    .update(updatedInfo)
+    .eq('id', updatedInfo.id);
+
+  if (error) {
+    throw new Error(`Error updating exhibit data: ${error.message}`);
+  }
+
+  const newExhibit = data;
+  return newExhibit;
+}
