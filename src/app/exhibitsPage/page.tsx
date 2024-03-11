b19148529165da884563549b0d5e624632447f3d
jxmoose <justin.xue7@gmail.com>
2024-03-10 17:57:20 -0700

    added database and page

---
+++ src/app/exhibitsPage/page.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { useEffect, useState} from 'react';
+import NavBar from '../../components/userComponents/navBar/navBar';
+import { ExhibitRow } from '../../types/types';
+import { fetchAllExhibits } from '../../supabase/exhibits/queries';
+
+function App() {
+    const [exhibits, setExhibits] = useState<ExhibitRow[]>([]);
+  useEffect(() => {
+    // Get exhibits
+    const getExhibits = async () => {
+      const fetchedExhibits: ExhibitRow[] = await fetchAllExhibits();
+      setExhibits(fetchedExhibits);
+    };
+    getExhibits(); 
+  }, [exhibits]);
+  return (
+    <div style={{ backgroundColor: '#ebf0e4', height: '100vh' }}>
+      <NavBar />
+      <div style={{ padding: '16px' }}>
+        <h1 style={{ color: '#333333', fontSize: '2rem', fontWeight: 700 }}>
+          Exhibits
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
