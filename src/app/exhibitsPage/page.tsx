f845353e0466fd99aa014d1735547b58d34eca8b
jxmoose <justin.xue7@gmail.com>
2024-03-10 18:41:09 -0700

    prettier

--- src/app/exhibitsPage/page.tsx
+++ src/app/exhibitsPage/page.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../../components/userComponents/navBar/navBar';
 import { ExhibitRow } from '../../types/types';
 import { fetchAllExhibits } from '../../supabase/exhibits/queries';
 
 function App() {
-    const [exhibits, setExhibits] = useState<ExhibitRow[]>([]);
+  const [exhibits, setExhibits] = useState<ExhibitRow[]>([]);
   useEffect(() => {
     // Get exhibits
     const getExhibits = async () => {
       const fetchedExhibits: ExhibitRow[] = await fetchAllExhibits();
       setExhibits(fetchedExhibits);
     };
-    getExhibits(); 
+    getExhibits();
   }, [exhibits]);
   return (
     <div style={{ backgroundColor: '#ebf0e4', height: '100vh' }}>
