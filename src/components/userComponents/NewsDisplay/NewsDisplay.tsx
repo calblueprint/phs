c7006c36ebee2cf2e3c7f0723d2474b52d05c962
jxmoose <justin.xue7@gmail.com>
2024-03-16 11:24:07 -0700

    style changes

--- src/components/userComponents/NewsDisplay/NewsDisplay.tsx
+++ src/components/userComponents/NewsDisplay/NewsDisplay.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment';
-import { FiExternalLink } from "react-icons/fi";
+import { FiExternalLink } from 'react-icons/fi';
 
 /**
  *
@@ -11,34 +11,39 @@ import { FiExternalLink } from "react-icons/fi";
  * @param root0.contentLink link to article
  * @returns Single Newsdisplay
  */
-export default function NewsDisplay(
-    {
-        title, 
-        createdAt, 
-        contentLink,
-        id
-    }:{
-        title: string;
-        createdAt: string;
-        contentLink: string;
-        id: string;
-    }) {
-    // format to readable date
-    const date = moment(createdAt).format("MMMM Do, YYYY");
-    return (
-        <li key={id}>
-            <a href={ contentLink }>
-                <div className="w-[95%] m-auto flex flex-col items-start gap-[12px] mb-[24px] mt-[24px] font-[Lato]">
-                    <div className="flex m-auto flex-row justify-between w-full"> 
-                        <p className="text-night text-[16px] not-italic font-medium leading-[normal] w-[90%]"> { title } </p>
-                        <div className="mt-[1px]"> 
-                            <FiExternalLink/>
-                        </div>
-                    </div>
-                    <p className="text-shadow not-italic font-light leading-[normal] uppercase text-xs"> { date } </p>
-                </div>
-            </a>
-            <hr className="border-silver w-[95%] m-auto h-[.5px] mb-[10px]"/>
-        </li>
-    );
-  };
+export default function NewsDisplay({
+  title,
+  createdAt,
+  contentLink,
+  id,
+}: {
+  title: string;
+  createdAt: string;
+  contentLink: string;
+  id: string;
+}) {
+  // format to readable date
+  const date = moment(createdAt).format('MMMM Do, YYYY');
+  return (
+    <li key={id}>
+      <a href={contentLink}>
+        <div className="w-[95%] m-auto flex flex-col items-start gap-[12px] mb-[24px] mt-[24px] font-[Lato]">
+          <div className="flex m-auto flex-row justify-between w-full">
+            <p className="text-night text-[16px] not-italic font-medium leading-[normal] w-[90%]">
+              {' '}
+              {title}{' '}
+            </p>
+            <div className="mt-[1px]">
+              <FiExternalLink />
+            </div>
+          </div>
+          <p className="text-shadow not-italic font-light leading-[normal] uppercase text-xs">
+            {' '}
+            {date}{' '}
+          </p>
+        </div>
+      </a>
+      <hr className="border-silver w-[95%] m-auto h-[.5px] mb-[10px]" />
+    </li>
+  );
+}
