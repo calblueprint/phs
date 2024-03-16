bbf2efe42cb84ef2ce6f387cdb54a95951ad853b
jxmoose <justin.xue7@gmail.com>
2024-03-16 14:33:29 -0700

    finished?

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -4,7 +4,7 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   public: {
@@ -48,39 +48,38 @@ export type Database = {
         }
         Relationships: [
           {
-            foreignKeyName: 'display_media_display_id_fkey';
-            columns: ['display_id'];
-            isOneToOne: false;
-            referencedRelation: 'displays';
-            referencedColumns: ['id'];
+            foreignKeyName: "display_media_display_id_fkey"
+            columns: ["display_id"]
+            isOneToOne: false
+            referencedRelation: "displays"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: 'display_media_media_id_fkey';
-            columns: ['media_id'];
-            isOneToOne: false;
-            referencedRelation: 'media';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
+            foreignKeyName: "display_media_media_id_fkey"
+            columns: ["media_id"]
+            isOneToOne: false
+            referencedRelation: "media"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       displays: {
         Row: {
-          coordinates: Json | null;
-          created_at: string;
-          description: string;
-          id: string;
-          title: string;
-          updated_at: string | null;
-        };
+          coordinates: Json | null
+          created_at: string
+          description: string
+          id: string
+          title: string
+          updated_at: string | null
+        }
         Insert: {
-          coordinates?: Json | null;
-          created_at?: string;
-          description?: string;
-          id: string;
-          summary?: string | null;
-          title?: string;
-          updated_at?: string | null;
-        };
+          coordinates?: Json | null
+          created_at?: string
+          description?: string
+          id: string
+          title?: string
+          updated_at?: string | null
+        }
         Update: {
           coordinates?: Json | null;
           created_at?: string;
@@ -94,21 +93,21 @@ export type Database = {
       };
       media: {
         Row: {
-          created_at: string;
-          id: string;
-          text: string | null;
-          title: string | null;
-          type: string | null;
-          url: string;
-        };
+          created_at: string
+          id: string
+          text: string | null
+          title: string | null
+          type: string | null
+          url: string
+        }
         Insert: {
-          created_at?: string;
-          id?: string;
-          text?: string | null;
-          title?: string | null;
-          type?: string | null;
-          url?: string;
-        };
+          created_at?: string
+          id?: string
+          text?: string | null
+          title?: string | null
+          type?: string | null
+          url?: string
+        }
         Update: {
           created_at?: string;
           id?: string;
@@ -121,97 +120,97 @@ export type Database = {
       };
       spotlight_recommendations: {
         Row: {
-          source_display_id: string;
-          target_display_id: string;
-        };
+          source_display_id: string
+          target_display_id: string
+        }
         Insert: {
-          source_display_id: string;
-          target_display_id: string;
-        };
+          source_display_id: string
+          target_display_id: string
+        }
         Update: {
-          source_display_id?: string;
-          target_display_id?: string;
-        };
+          source_display_id?: string
+          target_display_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: 'spotlight_recommendations_source_display_id_fkey';
-            columns: ['source_display_id'];
-            isOneToOne: false;
-            referencedRelation: 'tours';
-            referencedColumns: ['id'];
+            foreignKeyName: "spotlight_recommendations_source_display_id_fkey"
+            columns: ["source_display_id"]
+            isOneToOne: false
+            referencedRelation: "tours"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: 'spotlight_recommendations_target_display_id_fkey';
-            columns: ['target_display_id'];
-            isOneToOne: false;
-            referencedRelation: 'tours';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
+            foreignKeyName: "spotlight_recommendations_target_display_id_fkey"
+            columns: ["target_display_id"]
+            isOneToOne: false
+            referencedRelation: "tours"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       tour_displays: {
         Row: {
-          display_id: string;
-          display_order: number | null;
-          tour_id: string;
-        };
+          display_id: string
+          display_order: number | null
+          tour_id: string
+        }
         Insert: {
-          display_id: string;
-          display_order?: number | null;
-          tour_id: string;
-        };
+          display_id: string
+          display_order?: number | null
+          tour_id: string
+        }
         Update: {
-          display_id?: string;
-          display_order?: number | null;
-          tour_id?: string;
-        };
+          display_id?: string
+          display_order?: number | null
+          tour_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: 'tour_displays_display_id_fkey';
-            columns: ['display_id'];
-            isOneToOne: false;
-            referencedRelation: 'displays';
-            referencedColumns: ['id'];
+            foreignKeyName: "tour_displays_display_id_fkey"
+            columns: ["display_id"]
+            isOneToOne: false
+            referencedRelation: "displays"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: 'tour_displays_tour_id_fkey';
-            columns: ['tour_id'];
-            isOneToOne: false;
-            referencedRelation: 'tours';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
+            foreignKeyName: "tour_displays_tour_id_fkey"
+            columns: ["tour_id"]
+            isOneToOne: false
+            referencedRelation: "tours"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       tour_media: {
         Row: {
-          media_id: string;
-          tour_id: string;
-        };
+          media_id: string
+          tour_id: string
+        }
         Insert: {
-          media_id: string;
-          tour_id: string;
-        };
+          media_id: string
+          tour_id: string
+        }
         Update: {
-          media_id?: string;
-          tour_id?: string;
-        };
+          media_id?: string
+          tour_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: 'tour_media_media_id_fkey';
-            columns: ['media_id'];
-            isOneToOne: false;
-            referencedRelation: 'media';
-            referencedColumns: ['id'];
+            foreignKeyName: "tour_media_media_id_fkey"
+            columns: ["media_id"]
+            isOneToOne: false
+            referencedRelation: "media"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: 'tour_media_tour_id_fkey';
-            columns: ['tour_id'];
-            isOneToOne: false;
-            referencedRelation: 'tours';
-            referencedColumns: ['id'];
-          },
-        ];
-      };
+            foreignKeyName: "tour_media_tour_id_fkey"
+            columns: ["tour_id"]
+            isOneToOne: false
+            referencedRelation: "tours"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       tours: {
         Row: {
           category: Database['public']['Enums']['tour_category'];
@@ -250,53 +249,48 @@ export type Database = {
       };
     };
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      fetch_recommended_spotlights: {
-        Args: {
-          source_spotlight_id: string;
-        };
-        Returns: {
-          id: string;
-          name: string;
-          description: string;
-          created_at: string;
-          stop_count: number;
-          spotlight: boolean;
-          preview_text: string;
-          coordinates: Json;
-          category: Database['public']['Enums']['tour_category'];
-        }[];
-      };
       fetchimagesfordisplay: {
         Args: {
-          displayid: string;
-        };
+          displayid: string
+        }
         Returns: {
-          id: string;
-          url: string;
-          type: string;
-          title: string;
-          text: string;
-          created_at: string;
-        }[];
-      };
+          id: string
+          url: string
+          type: string
+          title: string
+          text: string
+          created_at: string
+        }[]
+      }
       fetchimagesfortour: {
         Args: {
-          tourid: string;
-        };
-          tourid: string;
-        };
+          tourid: string
+        }
         Returns: {
-          id: string;
-          url: string;
-          type: string;
-          title: string;
-          text: string;
-          created_at: string;
-        }[];
-      };
+          id: string
+          url: string
+          type: string
+          title: string
+          text: string
+          created_at: string
+        }[]
+      }
+      get_spotlight_recommendations: {
+        Args: {
+          source_display_id: string
+        }
+        Returns: {
+          id: string
+          url: string
+          type: string
+          title: string
+          text: string
+          created_at: string
+        }[]
+      }
       get_category_color: {
         Args: {
           category_in: string
@@ -349,7 +343,7 @@ export type Database = {
         }[];
       };
       join_tours_with_media: {
-        Args: Record<PropertyKey, never>;
+        Args: Record<PropertyKey, never>
         Returns: {
           id: string;
           name: string;
@@ -363,7 +357,7 @@ export type Database = {
       };
     };
     Enums: {
-      media_type: 'image' | 'video' | 'link';
+      media_type: "image" | "video" | "link"
       tour_category:
         | 'BuildingsAndServices'
         | 'ParksAviariesEnclosures'
@@ -386,12 +380,9 @@ export type Tables<
         Database[PublicTableNameOrOptions['schema']]['Views'])
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
-      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
-      Row: infer R;
-  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
-      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
-      Row: infer R;
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
     }
     ? R
     : never
@@ -413,18 +404,18 @@ export type TablesInsert<
     ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
-      Insert: infer I;
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
-  ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
-      Insert: infer I;
-    }
-    ? I
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -434,18 +425,18 @@ export type TablesUpdate<
     ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
-      Update: infer U;
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
-  ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
-      Update: infer U;
-    }
-    ? U
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
