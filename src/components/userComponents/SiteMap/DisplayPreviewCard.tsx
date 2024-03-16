b002c7bfcf7a2e3b298df4c93f12c5c6c1a15a06
Miha <athmiha@berkeley.edu>
2024-03-16 11:18:52 -0700

    styling r1 done with map popups

--- src/components/userComponents/SiteMap/DisplayPreviewCard.tsx
+++ src/components/userComponents/SiteMap/DisplayPreviewCard.tsx
@@ -58,27 +58,23 @@ function DisplayPreviewCard({
   /** route this to spotlights */
 
   return (
+   
     <Link href={`/spotlightPage/${id}`}>
       <div className="flex flex-col items-center justify-center w-[25rem] h-[8.25rem] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto p-4 pb-[2.87rem] pr-[1.19rem] pl-[1.19rem] rounded-md">
         <div
           className="flex flex-row items-center rounded-md overflow-hidden bg-ivory cursor-pointer w-full sm:w-4/4 md:w-5/5 lg:w-2/2 xl:w-5/5 flex-shrink-0 shadow-xl" 
-          // style={{ width: '22rem', height: '8.25rem', flexShrink: 0, borderRadius: '0.5rem', boxShadow: '0px 4px 24px 0px rgba(20, 20, 20, 0.16)' }}
+          
           aria-hidden="true"
         >
           {!loading && 
           <div className="relative w-[7.8125rem] z-10 h-[8.25rem] shrink-0 rounded-tl-md rounded-tr-none rounded-br-none rounded-bl-md">
            
             <Image
-              // className="shrink"
+            
               src={previewImage}
               alt='placeholder'
-              // width={100%} // Example width
-              // height={200} // Example height
-              // "relative w-[7.8125rem] h-[7rem] flex-none "
               layout="fill"
               objectFit="cover"
-              // objectPosition='center'
-
             /> 
             </div>
           }
@@ -95,23 +91,19 @@ function DisplayPreviewCard({
           >
             <div className=""> 
               <h3
-                className="relative text-asparagus font-lato"
-                // style={{ fontWeight: 'bolder' }}
-                // "flex-grow p-1"
-                // overflow-hidden text-night truncate font-lato text-base font-bold leading-normal
+                className="relative truncate text-asparagus pr-[0.31rem] pl-[0.75rem] pt-[1.25rem] uppercase font-light text-xs leading-normal"
+  
               >
                 {category}
               </h3>
               <h3
-                className="relative truncate font-bold font-lato text-night pr-[0.31rem] pl-[0.75rem] pt-[2.31rem] pb-[0rem] text-base"
-                // style={{ fontWeight: 'bolder' }}
-                // "flex-grow p-1"
-                // overflow-hidden text-night truncate font-lato text-base font-bold leading-normal
+                className="relative truncate font-medium font-lato text-night pr-[0.31rem] pl-[0.75rem] pt-[0.30rem] pb-[0rem] text-base leading-normal"
+
               >
                 {name}
               </h3>
       
-              <h4 className="relative font-lato h-[2.0625rem] pr-[0.31rem] pt-[0.37rem] pl-[0.75rem] pb-[2.38rem] text-shadow line-clamp-2 text-sm">
+              <h4 className="relative font-lato h-[2rem] pr-[0.31rem] pt-[0rem] pl-[0.75rem] pb-[2.4rem] text-shadow line-clamp-2 text-sm">
                 {description}
               </h4>
                 
@@ -122,6 +114,7 @@ function DisplayPreviewCard({
         </div>
       </div>
     </Link>
+
   );
 }
 
