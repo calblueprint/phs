b005a090138e41c419ff049ff09b487b15800611
Miha <athmiha@berkeley.edu>
2024-03-17 02:11:31 -0700

    virutal map and exihibits are displayed now toggle works - need to fix closing button and stuff

--- src/components/userComponents/SiteMap/DisplayPreviewCard.tsx
+++ src/components/userComponents/SiteMap/DisplayPreviewCard.tsx
@@ -84,6 +84,8 @@ function DisplayPreviewCard({
    
     <Link href={`/spotlightPage/${id}`}>
       <div className="flex flex-col items-center justify-center w-[25rem] h-[8.25rem] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto p-4 pb-[2.87rem] pr-[1.19rem] pl-[1.19rem] rounded-md">
+        
+
         <div
           className="flex flex-row items-center rounded-md overflow-hidden bg-ivory cursor-pointer w-full sm:w-4/4 md:w-5/5 lg:w-2/2 xl:w-5/5 flex-shrink-0 shadow-xl" 
           
@@ -113,8 +115,13 @@ function DisplayPreviewCard({
             tabIndex={0}
           >
             <div className=""> 
+              <div className="flex justify-end items-center pt-2 pr-2" onClick={handleClose}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
+                <path d="M10.0523 11.0274L3.35083 4.32596M10.0524 4.32596L3.35083 11.0275" stroke="#272929" stroke-width="1.11692" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+              </div>
               <h3
-                className="relative truncate text-asparagus pr-[0.31rem] pl-[0.75rem] pt-[1.25rem] uppercase font-light text-xs leading-normal"
+                className="relative truncate text-asparagus pr-[0.31rem] pl-[0.75rem] pt-[0rem] uppercase font-light text-xs leading-normal"
   
               >
                 {category}
