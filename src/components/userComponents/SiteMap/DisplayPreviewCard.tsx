e0cd91262520de2665d1b71ef1ccf19da9a7fe35
Andrei Tan <73627326+andreisito@users.noreply.github.com>
2024-04-08 17:30:31 -0700

    Andrei/t sfix [WIP] (#46)
    * feat: changing to db funcs
    
    * chore: sync
    
    * feat: refactored DB funcs and adjusted code styling to meet TStypes standards
    
    * chore: linting sync
    
    * feat: resolved relevant TSstyling, linting, and other conventions post-rebase
    
    * chore: sync
    
    * chore: sync II
    
    * chore: changed name of spotlight query
    
    * fix: forgot to save query
    
    * chore: sync

--- src/components/userComponents/SiteMap/DisplayPreviewCard.tsx
+++ src/components/userComponents/SiteMap/DisplayPreviewCard.tsx
@@ -48,100 +48,104 @@ function DisplayPreviewCard({
   useEffect(() => {
     const fetchDetails = async () => {
       setLoading(true);
-  
-      let imageUrl = ''; 
-      let displayName = ''; 
-  
-      if ('name' in tour) { 
+
+      let imageUrl = '';
+      let displayName = '';
+
+      if ('name' in tour) {
         // Fetch images for a tour
         const images = await fetchImagesForTour(tour.id);
         if (images && images.length > 0) {
-          imageUrl = images[0].url; 
+          imageUrl = images[0].url;
         }
-        displayName = tour.name; 
+        displayName = tour.name;
       } else {
         // Handle as an ExhibitRow
-        const imageObj = await fetchExhibitImage(tour.id); 
+        const imageObj = await fetchExhibitImage(tour.id);
         if (imageObj) {
-          imageUrl = imageObj.image; 
+          imageUrl = imageObj.image;
         }
-        displayName = tour.title; 
+        displayName = tour.title;
       }
-  
+
       // Set state variables
       setPreviewImage(imageUrl);
       setname1(displayName);
       setLoading(false);
     };
-  
+
     fetchDetails();
   }, [tour]);
-  
 
   /** route this to spotlights */
 
   return (
-      <div className="flex flex-col items-center justify-center w-[25rem] h-[8.25rem] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto p-4 pb-[2.87rem] pr-[1.19rem] pl-[1.19rem] rounded-md">
-        <div
-          className="flex flex-row items-center rounded-md overflow-hidden bg-ivory cursor-pointer w-full sm:w-4/4 md:w-5/5 lg:w-2/2 xl:w-5/5 flex-shrink-0 shadow-xl" 
-          aria-hidden="true"
-        >
-          {!loading && 
+    <div className="flex flex-col items-center justify-center w-[25rem] h-[8.25rem] max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto p-4 pb-[2.87rem] pr-[1.19rem] pl-[1.19rem] rounded-md">
+      <div
+        className="flex flex-row items-center rounded-md overflow-hidden bg-ivory cursor-pointer w-full sm:w-4/4 md:w-5/5 lg:w-2/2 xl:w-5/5 flex-shrink-0 shadow-xl"
+        aria-hidden="true"
+      >
+        {!loading && (
           <div className="relative w-[7.8125rem] z-10 h-[8.25rem] shrink-0 rounded-tl-md rounded-tr-none rounded-br-none rounded-bl-md">
-           
             <Image
-            
               src={previewImage}
-              alt='placeholder'
+              alt="placeholder"
               layout="fill"
               objectFit="cover"
-            /> 
-            </div>
-          }
-          <div
-            className="justify-items-center align-middle z-20 overflow-hidden w-full h-full"
-            onClick={handleClick}
-            onKeyDown={e => {
-              if (handleClick && e.key === 'Enter') {
-                handleClick();
-              }
-            }}
-            role="button"
-            tabIndex={0}
-          >
-            <div className=""> 
-            <div className="flex justify-end items-center pt-2 pr-2" onClick={(e) => {
-                  e.stopPropagation(); // This prevents the click from propagating to the parent link
-                  handleClose(); // Then, your handleClose function is called
-                }}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="15" viewBox="0 0 14 15" fill="none">
-                    <path d="M10.0523 11.0274L3.35083 4.32596M10.0524 4.32596L3.35083 11.0275" stroke="#272929" strokeWidth="1.11692" strokeLinecap="round" strokeLinejoin="round"/>
-                  </svg>
-                </div>
-              <Link href={`/spotlightPage/${id}`}>
-              <h3
-                className="relative truncate text-asparagus pr-[0.31rem] pl-[0.75rem] pt-[0rem] uppercase font-light text-xs leading-normal"
-  
+            />
+          </div>
+        )}
+        <div
+          className="justify-items-center align-middle z-20 overflow-hidden w-full h-full"
+          onClick={handleClick}
+          onKeyDown={e => {
+            if (handleClick && e.key === 'Enter') {
+              handleClick();
+            }
+          }}
+          role="button"
+          tabIndex={0}
+        >
+          <div className="">
+            <div
+              className="flex justify-end items-center pt-2 pr-2"
+              onClick={e => {
+                e.stopPropagation(); // This prevents the click from propagating to the parent link
+                handleClose(); // Then, your handleClose function is called
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="15"
+                viewBox="0 0 14 15"
+                fill="none"
               >
+                <path
+                  d="M10.0523 11.0274L3.35083 4.32596M10.0524 4.32596L3.35083 11.0275"
+                  stroke="#272929"
+                  strokeWidth="1.11692"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <Link href={`/spotlightPage/${id}`}>
+              <h3 className="relative truncate text-asparagus pr-[0.31rem] pl-[0.75rem] pt-[0rem] uppercase font-light text-xs leading-normal">
                 {category}
               </h3>
-              <h3
-                className="relative truncate font-medium font-lato text-night pr-[0.31rem] pl-[0.75rem] pt-[0.30rem] pb-[0rem] text-base leading-normal"
-
-              >
+              <h3 className="relative truncate font-medium font-lato text-night pr-[0.31rem] pl-[0.75rem] pt-[0.30rem] pb-[0rem] text-base leading-normal">
                 {name1}
               </h3>
-      
+
               <h4 className="relative font-lato h-[2rem] pr-[0.31rem] pt-[0rem] pl-[0.75rem] pb-[2.4rem] text-shadow line-clamp-2 text-sm">
                 {description}
               </h4>
-              </Link>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
-
-
+    </div>
   );
 }
 
