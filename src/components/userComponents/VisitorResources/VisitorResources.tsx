8a6e82d1ef3ba3f55c9d27ec29591a8ee1311a63
Andrei Tan <73627326+andreisito@users.noreply.github.com>
2024-04-08 19:01:16 -0700

    chore: switching back to div

--- src/components/userComponents/VisitorResources/VisitorResources.tsx
+++ src/components/userComponents/VisitorResources/VisitorResources.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { PiQrCodeBold } from 'react-icons/pi';
-import { FiMap } from 'react-icons/fi';
+import { FiMap, FiCompass } from 'react-icons/fi';
+import { MdOutlineCalendarMonth } from "react-icons/md";
 import Link from 'next/link';
 
 /**
@@ -9,14 +10,34 @@ import Link from 'next/link';
  */
 function VisitorResources() {
   return (
-    <div className="Visitor-Resources w-full h-48 bg-mint-cream pt-6 px-4">
-      <h3 className="text-night mb-4">Visitor Resources</h3>
+    <div 
+    // src="https://qkkuacqtcsfjbnzmxmhk.supabase.co/storage/v1/object/public/images/AnimalWellbeing?t=2024-04-09T01%3A52%3A17.540Z"
+    className="Visitor-Resources w-full h-98 bg-hunter-green px-4 py-20 
+flex-col justify-start items-start gap-4 inline-flex">
+      <p className="text-ivory text-2xl font-medium">Visitor Resources</p>
+      <Link 
+      href= "/exhibitPage"
+      className="ExhibitButton w-96 h-28 px-7 py-6 bg-stone-50 rounded-lg shadow
+       justify-start items-center gap-4 inline-flex">
+          <div className="GreenCircle w-14 h-14 relative bg-mint-cream rounded-full flex justify-center items-center">
+              <FiMap className="w-6 h-6 text-scary-forest" />
+          </div>
+          <div className="TextField flex-col gap-[4px]">
+              <p className="b1 text-neutral-700 leading-tight">
+                Explore our Exhibits
+              </p>
+              <p className="s w-52 text-zinc-500 text-sm font-normal font-['Lato']">
+                Discover interactive exhibits of our wildlife care center
+              </p>
+          </div>
+      </Link>
       <div className="flex space-x-6">
         <Link
-          className="w-44 h-24 bg-ivory text-night items-center rounded-md shadow inline-flex justify-around"
+          className="w-44 h-24 bg-ivory text-night items-center
+          rounded-md shadow inline-flex justify-around"
           href="/siteMapPage"
         >
-          <FiMap className="w-6 h-6 text-scary-forest" />
+          <FiCompass className="w-6 h-6 text-scary-forest" />
           <div className="w-23.5 h-12.25 flex-col justify-start items-start gap-0.5 inline-flex">
             <div className="b1 text-night">Site Map</div>
             <div className="w-24 text-left text-zinc-500 text-xs font-normal">
@@ -25,12 +46,14 @@ function VisitorResources() {
           </div>
         </Link>
         <Link
-          className="ml-auto w-44 h-24 bg-ivory text-night items-center rounded-md shadow inline-flex justify-around"
+          className="ml-auto w-44 h-24 bg-ivory text-night 
+          items-center rounded-md shadow inline-flex justify-around"
           href="/qrCodeTourPage"
         >
-          <PiQrCodeBold className="w-7 h-7 text-scary-forest" />
-          <div className="w-23.5 h-12.25 flex-col justify-start items-start gap-0.5 inline-flex">
-            <div className="b1 text-night">QR Explore</div>
+          <MdOutlineCalendarMonth className="w-7 h-7 text-scary-forest" />
+          <div className="w-23.5 h-12.25 flex-col justify-start
+           items-start gap-0.5 inline-flex">
+            <div className="b1 text-night">Hours & Location</div>
             <div className="w-24 text-left text-zinc-500 text-xs font-normal">
               Learn more about exhibits
             </div>
