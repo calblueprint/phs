37f9bfdce7f5b09e3333133e02622955e1209fea
jxmoose <justin.xue7@gmail.com>
2024-04-09 23:53:31 -0700

    design

--- src/components/userComponents/BackButton/page.tsx
+++ src/components/userComponents/BackButton/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { IoIosArrowRoundBack } from 'react-icons/io';
 
 /**
  * @param evt on click of button
@@ -18,7 +17,21 @@ export default function BackButton() {
   return (
     <button type="button" onClick={goBack} className="text-scary-forest">
       {' '}
-      <IoIosArrowRoundBack size={40} />
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+      >
+        <path
+          d="M17.5 10H2.5M2.5 10L6.66667 14.1667M2.5 10L6.66667 5.83337"
+          stroke="#386131"
+          strokeWidth="1.65"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
     </button>
   );
 }
--- src/components/userComponents/NewsDisplay/NewsDisplay.tsx
+++ src/components/userComponents/NewsDisplay/NewsDisplay.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import moment from 'moment';
-import { FiExternalLink } from 'react-icons/fi';
 
 /**
  *
@@ -34,12 +33,27 @@ export default function NewsDisplay({
               {title}{' '}
             </p>
             <div className="mt-[1px]">
-              <FiExternalLink />
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="11"
+                viewBox="0 0 12 11"
+                fill="none"
+              >
+                <path
+                  d="M11 0.5V0H11.5V0.5H11ZM5.47168 6.73591C5.27643 6.93118 4.95985 6.93119 4.76458 6.73594C4.56931 6.54068 4.56929 6.2241 4.76455 6.02883L5.47168 6.73591ZM10.5 5.20589V0.5H11.5V5.20589H10.5ZM11 1H6.29449V0H11V1ZM11.3536 0.853539L5.47168 6.73591L4.76455 6.02883L10.6464 0.146461L11.3536 0.853539Z"
+                  fill="#808080"
+                />
+                <path
+                  d="M10.4111 7.55886V7.55886C10.4111 8.32518 10.4111 8.70834 10.3131 9.01919C10.1054 9.67812 9.58921 10.1943 8.93028 10.402C8.61943 10.5 8.23627 10.5 7.46995 10.5H5.23541C3.23887 10.5 2.24061 10.5 1.62036 9.8798C1.00012 9.25955 1.00012 8.26129 1.00012 6.26475V4.0292C1.00012 3.26311 1.00012 2.88006 1.09807 2.56929C1.30582 1.91021 1.82207 1.39396 2.48115 1.18621C2.79192 1.08826 3.17497 1.08826 3.94106 1.08826V1.08826"
+                  stroke="#808080"
+                  strokeLinecap="round"
+                />
+              </svg>
             </div>
           </div>
-          <p className="text-shadow not-italic font-light leading-[normal] uppercase text-xs">
-            {' '}
-            {date}{' '}
+          <p className="text-shadow leading-[normal] text-xs font-normal font-[Lato] uppercase">
+            {date}
           </p>
         </div>
       </a>
