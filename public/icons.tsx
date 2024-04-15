2490f6b60603a06ec740b36418ad7a3b36d8d9dd
Chris Torres <christophertorres@berkeley.edu>
2024-04-15 01:37:07 -0700

    icons

--- public/icons.tsx
+++ public/icons.tsx
@@ -1,33 +1,116 @@
 import React from 'react';
 
 /**
- * @returns Hamburger menu icon fro NavBar component.
+ * @returns Hamburger menu icon for NavBar component.
  */
-export function MenuIcon() {
+export function HamburgerMenu() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="46"
-      height="46"
-      viewBox="0 0 46 46"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
       fill="none"
     >
       <path
-        d="M9.58331 13.4167H36.4166"
-        stroke="white"
-        strokeWidth="3.83333"
+        d="M20.0007 28.3332L31.6673 28.3332M8.33398 19.9998H31.6673M8.33398 11.6665H31.6673"
+        stroke="#FFFDF7"
+        strokeWidth="3"
         strokeLinecap="round"
+        strokeLinejoin="round"
       />
+    </svg>
+  );
+}
+
+/**
+ * @returns the CloseMenu icon
+ */
+export function CloseMenu() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+    >
       <path
-        d="M9.58331 23H36.4166"
-        stroke="white"
-        strokeWidth="3.83333"
+        d="M21.8754 21.8749L3.12549 3.125M21.8755 3.125L3.12549 21.8751"
+        stroke="#FFFDF7"
+        strokeWidth="3"
         strokeLinecap="round"
+        strokeLinejoin="round"
       />
+    </svg>
+  );
+}
+
+/**
+ * @param root0 -
+ * @param root0.strokeColor - The color of the arrow
+ * @returns the BackArrow icon
+ */
+export function BackArrow({strokeColor = "#386131"}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
       <path
-        d="M9.58331 32.5833H36.4166"
-        stroke="white"
-        strokeWidth="3.83333"
+        d="M15.75 9H2.25M2.25 9L6 12.75M2.25 9L6 5.25"
+        stroke={strokeColor}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+/**
+ * @returns the NextArrow icon
+ */
+export function NextArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <path
+        d="M2.25 9H15.75M15.75 9L12 5.25M15.75 9L12 12.75"
+        stroke="#FFFFFF"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+/**
+ * @returns the Caret Left icon
+ */
+export function CaretLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="23"
+      viewBox="0 0 15 23"
+      fill="none"
+    >
+      <path
+        d="M13 2L3 11.5L13 21"
+        stroke="#386131"
+        strokeOpacity="0.17"
+        strokeWidth="3.16667"
         strokeLinecap="round"
       />
     </svg>
@@ -35,24 +118,93 @@ export function MenuIcon() {
 }
 
 /**
- * @returns Back arrow for BackButton component.
+ * @returns the Caret Right icon
  */
-export function BackIcon() {
+export function CaretRight() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
+      width="15"
+      height="23"
+      viewBox="0 0 15 23"
       fill="none"
     >
       <path
-        d="M17.5 10H2.5M2.5 10L6.66667 14.1667M2.5 10L6.66667 5.83337"
-        stroke="#386131"
-        strokeWidth="1.65"
+        d="M2 2L12 11.5L2 21"
+        stroke="#D7E1CE"
+        strokeOpacity="0.22"
+        strokeWidth="3.16667"
         strokeLinecap="round"
-        strokeLinejoin="round"
       />
+    </svg>
+  );
+}
+
+/**
+ * @returns the Congratulations icon
+ */
+export function Congratulations() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="74"
+      height="38"
+      viewBox="0 0 74 38"
+      fill="none"
+    >
+      <path
+        d="M16.8928 32.3137L5.5791 21"
+        stroke="url(#paint0_linear_3790_3714)"
+        strokeWidth="9.87583"
+        strokeLinecap="round"
+      />
+      <path
+        d="M36.5791 21V5"
+        stroke="url(#paint1_linear_3790_3714)"
+        strokeWidth="9.87583"
+        strokeLinecap="round"
+      />
+      <path
+        d="M57.1072 32.3301L68.4209 21.0164"
+        stroke="url(#paint2_linear_3790_3714)"
+        strokeWidth="9.87583"
+        strokeLinecap="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_3790_3714"
+          x1="5.93265"
+          y1="20.6464"
+          x2="17.2464"
+          y2="31.9602"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#7CA24E" />
+          <stop offset="1" stopColor="#7CA24E" stopOpacity="0.45" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_3790_3714"
+          x1="37.0791"
+          y1="5"
+          x2="37.0791"
+          y2="21"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#7CA24E" />
+          <stop offset="1" stopColor="#7CA24E" stopOpacity="0.45" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_3790_3714"
+          x1="68.7745"
+          y1="21.3699"
+          x2="57.4607"
+          y2="32.6836"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#7CA24E" />
+          <stop offset="1" stopColor="#7CA24E" stopOpacity="0.45" />
+        </linearGradient>
+      </defs>
     </svg>
   );
 }
