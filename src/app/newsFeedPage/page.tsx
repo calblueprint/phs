74e8f173c39cb47729cdfff8848c5ed2e54c50fb
jxmoose <justin.xue7@gmail.com>
2024-04-28 13:42:36 -0700

    news feed web

--- src/app/newsFeedPage/page.tsx
+++ src/app/newsFeedPage/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import BackButton from '../../components/userComponents/BackButton/page';
 import NavBar from '../../components/userComponents/navBar/navBar';
 import { NewsRow } from '../../types/types';
@@ -21,26 +22,67 @@ export default function App() {
     getNews();
   }, [news]);
 
+  const [windowWidth, setWindowWidth] = useState(1024);
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="bg-ivory h-screen">
+    <div className="bg-ivory">
       <NavBar />
-      <div className="p-4">
-        <BackButton />
-        <h1 className="text-night text-3xl font-bold mt-2 -ml-[1.8px]">
-          News
-        </h1>
-        <ul>
-          {news.map(article => (
-            <NewsDisplay
-              key={article.updated_at}
-              id={article.id}
-              contentLink={article.content_link}
-              createdAt={article.created_at}
-              title={article.title}
-            />
-          ))}
-        </ul>
-      </div>
+      {windowWidth < 1024 && (
+        <div>
+          <div className="p-4">
+            <BackButton />
+            <h1 className="text-night text-3xl font-bold mt-2 -ml-[1.8px]">
+              News
+            </h1>
+            <ul>
+              {news.map(article => (
+                <NewsDisplay
+                  key={article.updated_at}
+                  id={article.id}
+                  contentLink={article.content_link}
+                  createdAt={article.created_at}
+                  title={article.title}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+      {windowWidth >= 1024 && (
+        <div>
+          <div className="ml-[400px] mt-[120px]">
+            <p className="text-night">
+              {' '}
+              <Link href="/" className="text-scary-forest hover:underline">
+                {' '}
+                Home{' '}
+              </Link>{' '}
+              / News{' '}
+            </p>
+            <h1 className="text-night text-4xl font-bold mt-6">News</h1>
+            <div>
+              <ul className="ml-[21.875rem] ">
+                {news.map(article => (
+                  <NewsDisplay
+                    key={article.updated_at}
+                    id={article.id}
+                    contentLink={article.content_link}
+                    createdAt={article.created_at}
+                    title={article.title}
+                  />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
