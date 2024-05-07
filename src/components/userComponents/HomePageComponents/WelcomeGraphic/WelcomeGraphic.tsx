1fe6e173f2862bc0f5a54f8ba07661d93c85a46a
Andrei Tan <73627326+andreisito@users.noreply.github.com>
2024-05-07 14:10:20 -0700

    fix: misc changes to styling and fixing some console log errors

--- src/components/userComponents/HomePageComponents/WelcomeGraphic/WelcomeGraphic.tsx
+++ src/components/userComponents/HomePageComponents/WelcomeGraphic/WelcomeGraphic.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { useWebDeviceDetection } from '../../../../context/WindowWidthContext/WindowWidthContext';
 
 /**
- * @returns - Welcome graphic on the home page.  Button available for "start tour" leads to the featured tours page and flow
+ * @returns - Welcome graphic on the home page.
  */
 function WelcomeGraphic() {
   const isWebDevice = useWebDeviceDetection();
   return (
     <div
-      className="h-96 bg-neutral-900 bg-opacity-60 flex justify-center items-center relative"
+      className="h-96 web:h-[45.188rem] bg-neutral-900 bg-opacity-60 flex justify-center items-center relative"
       style={{
         backgroundImage: `url('https://qkkuacqtcsfjbnzmxmhk.supabase.co/storage/v1/object/public/images/EllisonCover.png?t=2023-11-05T03%3A22%3A44.379Z')`,
         backgroundSize: 'cover',
@@ -16,7 +16,6 @@ function WelcomeGraphic() {
       }}
     >
       <div className="w-80 h-38 flex-col justify-start items-center gap-1 inline-flex">
-        {/* <div className="flex-col justify-start items-center gap-1 flex"> */}
         <p className=" font-light text-center">WELCOME TO</p>
         <h1 className="text-center w-80">The Peninsula Humane</h1>
         <h1 className="text-center w-80">Society & SPCA</h1>
