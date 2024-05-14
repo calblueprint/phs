1668d9107af4d4ebb071577f9d354b4bbd5daf05
sarahhpeng <137237272+sarahhpeng@users.noreply.github.com>
2024-05-13 23:10:43 -0700

    Revert "Claire fixes  (#63)" (#64)
    This reverts commit 8faac52311bf19fe18e0986a7c4fde221c9b4601.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,6 @@ export default function RootLayout({
           crossOrigin=""
         />
         <script src="html5-qrcode.min.js" />
-        
       </head>
 
       <body className={`${inter.className} ${lato.className}`}>
