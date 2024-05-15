28dd113569b9e35f35008314dbc9df142bc23f0b
Chris Torres <christophertorres@berkeley.edu>
2024-05-15 03:45:01 -0700

    style: add last and next stop button click effects

--- src/components/userComponents/NextStopButton/NextStopButton.tsx
+++ src/components/userComponents/NextStopButton/NextStopButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { CaretRight } from '../../../../public/icons';
 
@@ -14,15 +14,20 @@ interface NextStopButtonProps {
  * @returns the Next Stop button
  */
 export default function NextStopButton({ text, link }: NextStopButtonProps) {
+  const [isClicked, setIsClicked] = useState(false);
+  const handleClick = () => {
+    setIsClicked(true);
+  };
   return (
     <Link
       href={link}
-      className="bg-asparagus w-[10.375rem] h-[4.375rem] rounded-lg flex items-center relative p-[1.56rem]"
+      className={`bg-asparagus w-[10.375rem] h-[4.375rem] rounded-lg flex items-center relative p-[1.56rem] ${
+        isClicked ? 'brightness-[.8]' : ''
+      }`}
+      onClick={handleClick}
     >
       <div className="flex items-center justify-end w-full gap-[1.31rem]">
-        <p className="b1 text-ivory">
-          {text}
-        </p>
+        <p className="b1 text-ivory">{text}</p>
         <CaretRight />
       </div>
     </Link>
